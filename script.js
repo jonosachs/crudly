2fa25fe392e0ba5605242e752036cec3c3cc940b
Jono Sachs <jonosachs@gmail.com>
2026-02-15 11:25:09 +1100

    first commit, basic ui

---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.querySelector("#myForm");
+const feed = document.getElementById("feed");
+
+let postid = 1;
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const content = document.getElementById("input").value;
+  createPost(content);
+}
+
+function createPost(content) {
+  // create new post with id and timestamp
+  const newPost = document.createElement("div");
+  const timestamp = new Date(Date.now()).toLocaleString();
+  newPost.id = `post-${postid}`;
+  newPost.className = "post";
+  newPost.innerHTML = `
+  <div class=post-head>
+    <small class="date">${timestamp}</small>
+    <i id="delete-btn-${postid}" class="fa-regular fa-rectangle-xmark right"></i>
+  </div>
+  <div class="post-text">
+    <p>"${content}"</p>
+  </div>
+  `;
+
+  // append post to end of feed
+  feed.appendChild(newPost);
+
+  // add delete button listener
+  const id = postid;
+  deleteBtn = document.getElementById(`delete-btn-${id}`);
+  deleteBtn.addEventListener("click", () => deletePostById(id));
+
+  // clear user input
+  document.getElementById("input").value = "";
+
+  // increment post id
+  postid++;
+}
+
+function deletePostById(id) {
+  post = document.getElementById(`post-${id}`);
+  feed.removeChild(post);
+}
